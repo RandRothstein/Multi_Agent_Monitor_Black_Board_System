a207c7e5eda20ea42d82a0c9e14ac2848213a95d
Rand Rothstein <randrothstein17@gmail.com>
2026-03-10 11:58:40 +0530

    Gemini-2.5-flash LLM model integrated to traffic_agent,Provides['Determiation','Estimate Deviation','Recommendation']

---
+++ model/structured_output_model.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+
+class StructuredOutput(BaseModel):
+    product_id: int = Field(description='The sku_id which was detected')
+    agent_name: str = Field(description='The name of the agent which is returning this response'),
+    metric_name: str = Field(description='The type of anomaly the sku_id shows')
+    metric_value: float = Field(description="Specifies how much its deviating from normal")
+    severity_score: str = Field(description="Based on severity of the case give a score from (0 - 1) where 1 being the highest severity")
+    finding_summary: str = Field(description="Give the summary of findings and also recomendation to solve this")
